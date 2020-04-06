a3342bcb07ad3554b300f32f81d740ebb444a7e3
Richard Clifford <richard.clifford@soluis.com>
2020-04-06 20:56:48 +0100

    using commas for autoformat

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
 
 // the main function is the starting point for all our flutter apps
-void main() => runApp(MaterialApp(home: Text("hello world")));
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("hello world"),
+      ),
+    ),
+  );
+}
