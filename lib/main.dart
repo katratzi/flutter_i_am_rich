3c405edbacca421919dcbc62813fd047a4271001
Richard Clifford <richard.clifford@soluis.com>
2020-04-06 21:30:00 +0100

    adding an image and center it

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,16 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey[500],
         appBar: AppBar(
           title: Text("I am rich"),
           backgroundColor: Colors.blueGrey[800],
         ),
-        backgroundColor: Colors.blueGrey[300],
+        body: Center(
+          child: Image(
+            image: NetworkImage('http://icypath.com/tron.jpg'),
+          ),
+        ),
       ),
     ),
   );
